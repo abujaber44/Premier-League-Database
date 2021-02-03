2eed5cead0e980cbd585f5d0299e464c83bb73b6
mjaber <abujaber44@gmail.com>
2021-02-03 18:38:42 -0500

    dispaly selected teams in grids side by side

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -3,6 +3,11 @@ const signUpForm = document.querySelector(".container")
 const inputFields = document.querySelectorAll(".input-text")
 const logoutBtn = document.querySelector(".logout-btn")
 const teamCollection = document.querySelector("#team-collection")
+const gridContainer = document.querySelector(".grid-container")
+const gridItem = document.querySelector(".grid-item")
+const leftGrid = document.querySelector(".left-grid")
+const centerGrid = document.querySelector(".center-grid")
+const rightGrid = document.querySelector(".right-grid")
 const teamNames = document.getElementsByClassName("team-name")
 const mainContainer = document.querySelector("main")
 let loggedIn = null
@@ -36,7 +41,7 @@ function renderLoggedInUser() {
         welcome.innerText = `Welcome ${loggedIn.name}!`
         hideSignUpForm()
         showLogOutBtn()
-            // showLikedTeams()
+        showLikedTeams()
     } else {
         alert("Please enter your name and email address");
     }
@@ -84,23 +89,51 @@ function addTeam(event) {
 function showLikedTeams() {
     user = loggedIn
     api.fetchUserTeams(user.id).then(data => {
-        data.map(team => {
-            teamCollection.innerHTML += `<div>
-                    <h2 class="team-name">${team.team_name} Players:</h2>`
-            team.players.map(player => {
-                teamCollection.innerHTML += `<div>
-                    <ul> Name: ${player.player_name} </ul>
-                    <li> Number: ${player.player_number} </li>
-                    <li> Country: ${player.player_country}</li>
-                    <li> Type: ${player.player_type}</li>
-                    <li> Age: ${player.player_age}</li>
-                    <li> Match Played: ${player.player_match_played}</li>
-                    <li> Goals: ${player.player_goals}</li>
-                    <li> Yellow Cards: ${player.player_yellow_cards}</li>
-                    <li> Red Cards: ${player.player_red_cards}</li>
-                    </br>
-                  </div>`
-            })
+        let team = data[0]
+        leftGrid.innerHTML += `<div class="grid-item">
+                                        <h2>${team.team_name} Players:</h2>
+                                         </div>`
+        team.players.map(player => {
+            leftGrid.innerHTML += `
+                    <div class="grid-item"> Name: ${player.player_name} <br>
+                                            Age: ${player.player_age} <br>
+                                            Number: ${player.player_number} <br>
+                                            Country: ${player.player_country} <br>
+                                            Type: ${player.player_type} <br>
+                                            Goals: ${player.player_goals} <br>
+                                             </div>`
+        })
+    })
+    api.fetchUserTeams(user.id).then(data => {
+        let team = data[1]
+        centerGrid.innerHTML += `<div class="grid-item">
+                                        <h2>${team.team_name} Players:</h2>
+                                         </div>`
+        team.players.map(player => {
+            centerGrid.innerHTML += `
+                    <div class="grid-item"> Name: ${player.player_name} <br>
+                                            Age: ${player.player_age} <br>
+                                            Number: ${player.player_number} <br>
+                                            Country: ${player.player_country} <br>
+                                            Type: ${player.player_type} <br>
+                                            Goals: ${player.player_goals} <br> 
+                                            </div>`
+        })
+    })
+    api.fetchUserTeams(user.id).then(data => {
+        let team = data[2]
+        rightGrid.innerHTML += `<div class="grid-item">
+                                        <h2>${team.team_name} Players:</h2>
+                                         </div>`
+        team.players.map(player => {
+            rightGrid.innerHTML += `
+                    <div class="grid-item"> Name: ${player.player_name} <br>
+                                            Age: ${player.player_age} <br>
+                                            Number: ${player.player_number} <br>
+                                            Country: ${player.player_country} <br>
+                                            Type: ${player.player_type} <br>
+                                            Goals: ${player.player_goals} <br> 
+                                            </div>`
         })
     })
 }
--- frontend/scripts/services/api.js
+++ frontend/scripts/services/api.js
@@ -69,6 +69,4 @@ class API {
     fetchAllteams = () => {
         return fetch(this.allteamsURL).then(this.parseJSON)
     }
-
-
 }
